e7557c393d2014b2a6173d16ac87b3f7da1f16ff
MotorTruck1221 <tuckerj0606@icloud.com>
2023-02-04 16:23:47 -0700

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [prefetch(), tailwind(), react(), robotsTxt()],
+  output: "server",
+  adapter: node({
+    mode: 'middleware'
+  })
+});

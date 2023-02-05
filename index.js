dd675c33a0b9b84d3622e2733b6a107eb5b2ad36
Hiro <support@weeb-central.icu>
2023-02-04 18:12:43 -0800

    404 page

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { handler as ssrHandler } from './dist/server/entry.mjs';
+import express from "express";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
 
 const app = express();
-app.use(express.static('dist/client/'))
+app.use(express.static("dist/client/"));
 app.use(ssrHandler);
 
 app.listen(8080);
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,52 +1,76 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Hamburger from 'hamburger-react'
-import { motion, AnimatePresence } from 'framer-motion';
+import React from "react";
+import { useState, useEffect } from "react";
+import Hamburger from "hamburger-react";
+import { motion, AnimatePresence } from "framer-motion";
 export default function Modal() {
-    const [modal, setModal] = useState(false);
-    useEffect(() => {
-        const modal = document.getElementById('modal');
-        const modalContent = document.getElementById('content');
-        const btn = document.getElementById('get-started');
-        const ham = document.getElementsByClassName('hamburger-react')[0];
-        const span = document.getElementsByClassName('close')[0];
-        //detect click outside modal
-        window.onclick = function (event) {
-            if (event.target === btn) {
-                setModal(true);
-            }
-            else if (event.target === ham) {
-                console.log('');
-            }
-            else {
-                setModal(false);
-            }
-        }
-    }, []); 
-    return (
-        <div>
-            <AnimatePresence>
-                {modal && (
-                    <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 0.5 }} exit={{ opacity: 0}} className="modal w-52" id="modal">
-                        <div className="modal-content text-right p-2" id="content">
-                            <ul className="modal-list">
-                                <li className="modal-list-item text-xl pr-4 font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
-                                    <a href="/" className="modal-list-item-link block">Home</a>
-                                </li>
-                                <li className="modal-list-item text-xl pr-4 font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
-                                    <a href="/proxy" className="modal-list-item-link block">Proxy</a>
-                                </li>
-                                <li className="modal-list-item pr-4 text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
-                                    <a href="https://auttaja.io/ruby" className="modal-list-item-link block">Discord</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-            <div className="h-14" id="hamburger">
-                <Hamburger toggled={modal} toggle={setModal} size={25} color={'var(--text-color)'} />
+  const [modal, setModal] = useState(false);
+  useEffect(() => {
+    const modal = document.getElementById("modal");
+    const modalContent = document.getElementById("content");
+    const btn = document.getElementById("get-started");
+    const ham = document.getElementsByClassName("hamburger-react")[0];
+    const span = document.getElementsByClassName("close")[0];
+    //detect click outside modal
+    window.onclick = function (event) {
+      if (event.target === btn) {
+        setModal(true);
+      } else if (event.target === ham) {
+        console.log("");
+      } else {
+        setModal(false);
+      }
+    };
+  }, []);
+  return (
+    <div>
+      <AnimatePresence>
+        {modal && (
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0 }}
+            className="modal w-52"
+            id="modal"
+          >
+            <div className="modal-content text-center p-2" id="content">
+              <ul className="modal-list">
+                <li className="modal-list-item text-xl pr-4 font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
+                  <a href="/" className="modal-list-item-link block">
+                    Home
+                  </a>
+                </li>
+                <li className="modal-list-item text-xl pr-4 font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
+                  <a href="/proxy" className="modal-list-item-link block">
+                    Proxy
+                  </a>
+                </li>
+                <li className="modal-list-item text-xl pr-4 font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
+                  <a href="/settings" className="modal-list-item-link block">
+                    Settings
+                  </a>
+                </li>
+                <li className="modal-list-item pr-4 text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200 py-2">
+                  <a
+                    href="https://auttaja.io/ruby"
+                    className="modal-list-item-link block"
+                  >
+                    Discord
+                  </a>
+                </li>
+              </ul>
             </div>
-        </div>
-    );
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div className="h-14" id="hamburger">
+        <Hamburger
+          toggled={modal}
+          toggle={setModal}
+          size={25}
+          color={"var(--text-color)"}
+        />
+      </div>
+    </div>
+  );
 }

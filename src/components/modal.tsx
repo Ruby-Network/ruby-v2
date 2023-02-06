51ac4c5a4bf1b07f6da19ec25132887e6f6adca6
MotorTruck1221 <tuckerj0606@icloud.com>
2023-02-05 18:08:07 -0700

    Yes

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Hamburger from 'hamburger-react';
 import { motion, AnimatePresence } from 'framer-motion';
+//import font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faWindowMaximize, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 export default function Modal() {
     const [modal, setModal] = useState(false);
     useEffect(() => {
@@ -26,10 +30,10 @@ export default function Modal() {
             <AnimatePresence>
                 {modal && (
                     <motion.div
-                        animate={{ opacity: 1 }}
-                        initial={{ opacity: 0 }}
+                        animate={{ x: '0', opacity: 1}}
+                        initial={{ x: '200%', opacity: 0 }}
                         transition={{ duration: 0.5 }}
-                        exit={{ opacity: 0 }}
+                        exit={{ x: '200%', opacity: 0 }}
                         className="modal w-52"
                         id="modal"
                     >
@@ -38,35 +42,47 @@ export default function Modal() {
                             id="content"
                         >
                             <ul className="modal-list">
-                                <li className="modal-list-item text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200">
+                                <li className="modal-list-item text-xl font-semibold rounded-xl hover:bg-[var(--bg-color)] hover:brightness-200 m-3">
                                     <a
                                         href="/"
-                                        className="modal-list-item-link block"
+                                        className="modal-list-item-link block w-full p-2 border-[var(--border-color)] border-2 border-solid rounded-xl"
                                     >
+                                        <FontAwesomeIcon icon={faHome} />
+                                        {' '}
+                                        {' '}
                                         Home
                                     </a>
                                 </li>
-                                <li className="modal-list-item text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200">
+                                <li className="modal-list-item text-xl font-semibold rounded-xl hover:bg-[var(--bg-color)] hover:brightness-200 m-3">
                                     <a
                                         href="/proxy"
-                                        className="modal-list-item-link block"
+                                        className="modal-list-item-link block w-full p-2 border-[var(--border-color)] border-2 border-solid rounded-xl"
                                     >
+                                        <FontAwesomeIcon icon={faWindowMaximize} />
+                                        {' '}
+                                        {' '}
                                         Proxy
                                     </a>
                                 </li>
-                                <li className="modal-list-item text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200">
+                                <li className="modal-list-item text-xl font-semibold hover:bg-[var(--bg-color)] hover:brightness-200 m-3 rounded-xl">
                                     <a
                                         href="/settings"
-                                        className="modal-list-item-link block"
+                                        className="modal-list-item-link block w-full p-2 border-[var(--border-color)] border-2 border-solid rounded-xl"
                                     >
+                                        <FontAwesomeIcon icon={faGear} />
+                                        {' '}
+                                        {' '}
                                         Settings
                                     </a>
                                 </li>
-                                <li className="modal-list-item text-xl font-semibold rounded-sm hover:bg-[var(--bg-color)] hover:brightness-200">
+                                <li className="modal-list-item text-xl font-semibold rounded-xl hover:bg-[var(--bg-color)] hover:brightness-200 m-3">
                                     <a
                                         href="https://auttaja.io/ruby"
-                                        className="modal-list-item-link block"
+                                        className="modal-list-item-link block w-full p-2 border-[var(--border-color)] border-2 border-solid rounded-xl"
                                     >
+                                        <FontAwesomeIcon icon={faDiscord} />
+                                        {' '}
+                                        {' '}
                                         Discord
                                     </a>
                                 </li>

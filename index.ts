de8663ea735e73bec73a9a61acbcf5e59df21443
MotorTruck1221 <tuckerj0606@icloud.com>
2023-02-17 08:30:10 -0700

    Try Catch block

--- index.ts
+++ index.ts
@@ -47,7 +47,17 @@ if (cluster.isPrimary) {
         //Get the url search parameters and check if it matches the key from the environment variable
         //only block /,/404,/apps,/error,/search,/settings and /index if the key or cookie is not present
         if (bare.shouldRoute(req)) {
-            bare.routeRequest(req, res);
+            try {
+                bare.routeRequest(req, res);
+            }
+            catch (error) {
+                console.error(error);
+                res.writeHead(302, {
+                    Location: '/error',
+                });
+                res.end();
+                return;
+            }
         }
         else if (url.search === `?${key}` && !req.headers.cookie?.includes(key)) {
             res.writeHead(302, {

12c03a4bdb536280cf106f835cc06753e90d13f1
MotorTruck1221 <tuckerj0606@icloud.com>
2023-02-18 02:15:26 -0700

    Make the add button move to the end of the list

--- public/js/customApps.js
+++ public/js/customApps.js
@@ -5,11 +5,11 @@ if (customApps) {
     let appElement = document.createElement('button');
     appElement.setAttribute('class', 'app-button hover:border-[var(--border-color)] hover:rounded-xl hover:border-2 active:brightness-200');
     appElement.innerHTML = `<ol><i class="fa-solid fa-user-plus"></i></ol> <p>${app.name}</p>`;
-    //add onclick event to the button
     appElement.onclick = () => {
         localStorage.setItem('reloaded', 'false'); 
         window.location.replace('/search' + '#custom=' + app.url);
     }
     document.getElementById('apps').appendChild(appElement);
+    document.getElementById('apps').appendChild(document.getElementById('add-app'));
   });
 }

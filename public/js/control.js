1a5ca3c11e3515d1bf765d2b19bca8884ed118c2
MotorTruck1221 <tuckerj0606@icloud.com>
2023-02-19 21:21:21 -0700

    Add nave bar auto for fullscreen

--- public/js/control.js
+++ public/js/control.js
@@ -3,7 +3,8 @@
 // let forward = document.getElementById('forward-control');
 // let fullscreen = document.getElementById('fullscreen-control');
 // let exit = document.getElementById('exit-control');
-
+let navToggle = document.getElementById('nav-toggle');
+let header = document.getElementById('header');
 function refreshIframe() {
     iframe.contentWindow.location.reload();
 }
@@ -13,34 +14,29 @@ function backIframe() {
 function forwardIframe() {
     iframe.contentWindow.history.forward();
 }
-//listen for a hover on the top of the iframe when the iframe is fullscreen
-//if the user hovers over the top of the iframe then show the controls
-//if the user hovers out of the top of the iframe then hide the controls
-// let control = document.getElementById('exit-iframe');
-// let fullscreen = localStorage.getItem('fulls||ruby');
-// if (fullscreen === 'true') {
-//     let iframe = document.getElementById('uv-iframe');
-//     iframe.addEventListener('mouseover', function () {
-//         control.classList.remove('dnone');
-//     });
-//     iframe.addEventListener('mouseout', function () {
-//         control.classList.add('dnone');
-//     });
-// }
+//add an event listener of hover to the navToggle
+navToggle.addEventListener('mouseover', function () {
+    let fullscreenBehavior = localStorage.getItem('fullscreenBehavior');
+    if (fullscreenBehavior === 'true') {
+        fullScreenIframe(false);
+    } else if (fullscreenBehavior === 'false') {
+        fullScreenIframe(false, 'content');
+    }
+});
 function fullScreenIframe(value, fullscreenBehavior) {
     if (fullscreenBehavior === 'content') {
         if (value === true) {
             let iframe = document.getElementById('uv-iframe');
-            localStorage.setItem('fulls||ruby', 'true');
             iframe.requestFullscreen();
+            navToggle.classList.remove('dnone');
         } else if (value === false) {
-            localStorage.setItem('fulls||ruby', 'false');
             document.exitFullscreen();
+            navToggle.classList.add('dnone');
         }
     } else {
         if (value === true) {
             let iframe = document.getElementById('uv-iframe');
-            localStorage.setItem('fulls||ruby', 'true');
+            navToggle.classList.remove('dnone');
             //set to position absolute
             iframe.style.position = 'absolute';
             //set to top left corner
@@ -50,12 +46,12 @@ function fullScreenIframe(value, fullscreenBehavior) {
             iframe.style.width = '100%';
             iframe.style.height = '100%';
             //set z-index to 9999
-            iframe.style.zIndex = '9999';
+            iframe.style.zIndex = '9998';
             //add a transition
             iframe.style.transition = 'all 0.5s ease-in-out';
         } else if (value === false) {
             let iframe = document.getElementById('uv-iframe');
-            localStorage.setItem('fulls||ruby', 'false');
+            navToggle.classList.add('dnone');
             //set styles to height: calc(100% - 4rem);width: 100%;border: none;position: fixed;top: 4rem;right: 0;left: 0;bottom: 0;border: none; background: var(--bg-color);
             iframe.style.height = 'calc(100% - 4rem)';
             iframe.style.width = '100%';
@@ -68,6 +64,7 @@ function fullScreenIframe(value, fullscreenBehavior) {
             iframe.style.border = 'none';
             iframe.style.background = 'var(--bg-color)';
             iframe.style.transition = 'all 0.5s ease-in-out';
+            iframe.style.zIndex = '2';
         }
     }
 }

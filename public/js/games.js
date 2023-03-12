bf8228f579ae3fc4dc3e71ec7df3781841339bc5
MotorTruck1221 <tuckerj0606@icloud.com>
2023-03-11 23:35:48 -0700

    Add CSS for search bar (in the near future)

--- public/js/games.js
+++ public/js/games.js
@@ -6,7 +6,7 @@ await fetch('/assets/games.json')
                 //make game.name lowercase and replace spaces with dashes
                 let gn = game.name.toLowerCase().replace(/\s/g, '-');
                 return `
-            <div onclick='window.location.href = "/games/${gn}"' class="game-tile hover:border-[var(--border-color)] hover:rounded-xl hover:border-2">
+            <div onclick='window.location.href = "/games/${gn}"' data-game="${gn}" class="game-tile hover:border-[var(--border-color)] hover:rounded-xl hover:border-2">
             <img src="${game.img}" alt="${game.name}" />
             <p> ${game.name} </p>
         </div>

6c3b8b8bb57c5332dddfb7225322e633c917eb87
MotorTruck1221 <tuckerj0606@icloud.com>
2023-03-29 22:43:48 -0600

    ADS

---
+++ public/ads/ads.js
@@ -0,0 +1,1 @@
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5837744,document.body||document.documentElement)

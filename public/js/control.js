51fe435d64855276b7e5c2c67b64663b4eb5f6d5
MotorTruck1221 <tuckerj0606@icloud.com>
2023-04-14 03:47:15 -0600

    FIX

--- public/js/control.js
+++ public/js/control.js
@@ -79,62 +79,6 @@ function fullscreenIframe() {
         fullScreenIframe(true);
     }
 }
-function fullScreenIframe(value, fullscreenBehavior) {
-    if (fullscreenBehavior === 'content') {
-        if (value === true) {
-            let iframe = document.getElementById('uv-iframe');
-            iframe.requestFullscreen();
-            navToggle.classList.remove('dnone');
-        } else if (value === false) {
-            document.exitFullscreen();
-            navToggle.classList.add('dnone');
-        }
-    } else {
-        if (value === true) {
-            let iframe = document.getElementById('uv-iframe');
-            navToggle.classList.remove('dnone');
-            //set to position absolute
-            iframe.style.position = 'absolute';
-            //set to top left corner
-            iframe.style.top = '0';
-            iframe.style.left = '0';
-            //set to full width and height
-            iframe.style.width = '100%';
-            iframe.style.height = '100%';
-            //set z-index to 9999
-            iframe.style.zIndex = '9998';
-            //add a transition
-            iframe.style.transition = 'all 0.5s ease-in-out';
-        } else if (value === false) {
-            let iframe = document.getElementById('uv-iframe');
-            navToggle.classList.add('dnone');
-            //set styles to height: calc(100% - 4rem);width: 100%;border: none;position: fixed;top: 4rem;right: 0;left: 0;bottom: 0;border: none; background: var(--bg-color);
-            iframe.style.height = 'calc(100% - 4rem)';
-            iframe.style.width = '100%';
-            iframe.style.border = 'none';
-            iframe.style.position = 'fixed';
-            iframe.style.top = '4rem';
-            iframe.style.right = '0';
-            iframe.style.left = '0';
-            iframe.style.bottom = '0';
-            iframe.style.border = 'none';
-            iframe.style.background = 'var(--bg-color)';
-            iframe.style.transition = 'all 0.5s ease-in-out';
-            iframe.style.zIndex = '2';
-        }
-    }
-}
-function fullscreenIframe() {
-    let fullscreenBehavior = localStorage.getItem('fullscreenBehavior');
-    if (fullscreenBehavior === 'true') {
-        fullScreenIframe(true);
-    } else if (fullscreenBehavior === 'false') {
-        fullScreenIframe(true, 'content');
-    } else {
-        localStorage.setItem('fullscreenBehavior', 'true');
-        fullScreenIframe(true);
-    }
-}
 function exitIframe() {
     if (
         window.location.href === '/games' ||
